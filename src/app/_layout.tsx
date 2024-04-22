05334c71b7e222ab9d6e0854c412960d03a75daf
chandra <chandrapenugonda655@gmail.com>
2024-04-22 16:11:03 +0530

    animated theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,11 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 import { Slot } from "expo-router";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <SafeAreaProvider>
+      <Slot />
+    </SafeAreaProvider>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,95 +1,114 @@
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  PixelRatio,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import {
+  Canvas,
+  Group,
+  Image,
+  Mask,
+  Rect,
+  SkImage,
+  makeImageFromView,
+} from "@shopify/react-native-skia";
+import { SystemBars } from "react-native-bars";
+import { useSharedValue, withTiming } from "react-native-reanimated";
+
+const sleep = async (timer: number) =>
+  new Promise((resolve) => setTimeout(resolve, timer));
 
 export default function Page() {
-  return (
-    <View className="flex flex-1">
-      <Header />
-      <Content />
-      <Footer />
-    </View>
-  );
-}
-
-function Content() {
-  return (
-    <View className="flex-1">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <View className="px-4 md:px-6">
-          <View className="flex flex-col items-center gap-4 text-center">
-            <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
-            >
-              Welcome to Project ACME
-            </Text>
-            <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
-              Discover and collaborate on amce. Explore our services now.
-            </Text>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/"
-              >
-                Explore
-              </Link>
-            </View>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-function Header() {
-  const { top } = useSafeAreaInsets();
-  return (
-    <View style={{ paddingTop: top }}>
-      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
-          ACME
-        </Link>
-        <View className="flex flex-row gap-4 sm:gap-6">
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            About
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Product
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Pricing
-          </Link>
-        </View>
-      </View>
-    </View>
-  );
-}
-
-function Footer() {
-  const { bottom } = useSafeAreaInsets();
+  const ref = useRef();
+  const pd = PixelRatio.get();
+  const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = useWindowDimensions();
+  const insets = useSafeAreaInsets();
+  const [theme, setTheme] = useState("light");
+  const [overlay, setOverlay] = useState<SkImage | null>(null);
+  const themeStyles = theme === "light" ? lightTheme : darkTheme;
+  const mask = useSharedValue(0);
+  const [active, setActive] = useState(false);
+  const handlePress = async () => {
+    if (active) return;
+    setActive(true);
+    setTheme(theme === "light" ? "dark" : "light");
+    const snapshot = await makeImageFromView(ref);
+    setOverlay(snapshot);
+    await sleep(80);
+    mask.value = withTiming(SCREEN_WIDTH, { duration: 800 });
+    await sleep(800);
+    setOverlay(null);
+    mask.value = 0;
+    setActive(false);
+  };
   return (
     <View
-      className="flex shrink-0 bg-gray-100 native:hidden"
-      style={{ paddingBottom: bottom }}
+      className="flex flex-1"
+      style={[{ paddingTop: insets.top }, themeStyles.container]}
+      ref={ref}
+      collapsable={false}
     >
-      <View className="py-6 flex-1 items-start px-4 md:px-6 ">
-        <Text className={"text-center text-gray-700"}>
-          © {new Date().getFullYear()} Me
-        </Text>
-      </View>
+      <SystemBars
+        animated={true}
+        barStyle={theme === "dark" ? "light-content" : "dark-content"}
+      />
+      <Header theme={theme} handlePress={handlePress} />
+      {overlay && (
+        <Canvas pointerEvents="none" style={StyleSheet.absoluteFillObject}>
+          <Mask
+            mode="luminance"
+            mask={
+              <Group>
+                <Rect
+                  height={SCREEN_HEIGHT}
+                  width={SCREEN_WIDTH}
+                  color="white"
+                />
+                <Rect height={SCREEN_HEIGHT} width={mask} color="black" />
+              </Group>
+            }
+          >
+            <Image
+              image={overlay}
+              width={overlay.width() / pd}
+              height={overlay.height() / pd}
+              x={0}
+              y={0}
+            />
+          </Mask>
+        </Canvas>
+      )}
     </View>
   );
 }
+
+const Header = ({ theme, handlePress }) => {
+  return (
+    <View className="p-5 border-b border-gray-200">
+      <TouchableOpacity onPress={handlePress} activeOpacity={1}>
+        <MaterialIcons
+          name={theme === "light" ? "dark-mode" : "light-mode"}
+          size={24}
+          color={theme === "light" ? "black" : "white"}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const darkTheme = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+  },
+});
+const lightTheme = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+  },
+});
